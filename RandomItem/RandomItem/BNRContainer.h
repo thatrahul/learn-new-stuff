51916769fbb4b3b3a95fc6adaae51af37591b655
Rahul Phadnis <rahul.phadnis@magnet.com>
2014-10-28 23:03:43 -0700

    container

---
+++ RandomItem/RandomItem/BNRContainer.h
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.h
+//  RandomItem
+//
+//  Created by Rahul Phadnis on 10/28/14.
+//  Copyright (c) 2014 Rahul Phadnis. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@interface BNRContainer : BNRItem
+
+@end
---
+++ RandomItem/RandomItem/BNRContainer.m
@@ -0,0 +1,13 @@
+//
+//  BNRContainer.m
+//  RandomItem
+//
+//  Created by Rahul Phadnis on 10/28/14.
+//  Copyright (c) 2014 Rahul Phadnis. All rights reserved.
+//
+
+#import "BNRContainer.h"
+
+@implementation BNRContainer
+
+@end
