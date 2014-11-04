c8ec405f49d571750c3ee3b7f92e5253a3544f62
Rahul Phadnis <rahul.phadnis@magnet.com>
2014-11-04 13:42:37 -0800

    touch handling

--- ConcentricCircles/BNRConcentricView.m
+++ ConcentricCircles/BNRConcentricView.m
@@ -8,12 +8,19 @@
 
 #import "BNRConcentricView.h"
 
+@interface BNRConcentricView ()
+
+@property (strong, nonatomic, setter=mySetColor:) UIColor *circleColor;
+
+@end
+
 @implementation BNRConcentricView
 
 -(instancetype) initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
+        self.circleColor = [UIColor purpleColor];
     }
     return self;
 }
@@ -26,18 +33,20 @@
     center.x = bounds.origin.x + (bounds.size.width/2.0);
     center.y = bounds.origin.y + (bounds.size.height/2.0);
     
-    float radius = (MIN (bounds.size.height, bounds.size.width)-10)/2.0;
+    //float radius = (MIN (bounds.size.height, bounds.size.width)-10)/2.0;
     
     float maxRadius = hypot(bounds.size.width, bounds.size.height)/ 2.0;
     
     UIBezierPath *path =[UIBezierPath new];
     path.lineWidth=5;
     //set the color
-    [[UIColor greenColor] setStroke];
+    [self.circleColor setStroke];
     //path.co
     
     for (float currentRadius = maxRadius ; currentRadius >0 ; currentRadius=currentRadius-20) {
+        //move the pen
         [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        //add the circle
         [path addArcWithCenter:center radius:currentRadius startAngle:0 endAngle:M_PI*2 clockwise:YES];
     }
     
@@ -46,6 +55,26 @@
     //draw the line
     [path stroke];
 }
+//event when fingers touch the screen
+- (void)touchesBegan:(NSSet *)touches
+           withEvent:(UIEvent *)event {
+    NSLog(@"%@ was touched", self);
+    
+    float red = (arc4random() % 100)/100.0;
+    float green = (arc4random() % 100)/100.0;
+    float blue = (arc4random() % 100)/100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    
+    self.circleColor=randomColor;
+    
+}
 
+-(void) mySetColor : (UIColor *) newColor
+{
+    _circleColor = newColor;
+    NSLog(@"Updated color %@", _circleColor);
+    [self setNeedsDisplay];
+}
 
 @end
